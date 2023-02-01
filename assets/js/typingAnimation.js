a23e0eac198f7654faf100b927530104817231df
Navin Ruas <navinbr@sempreceub.com>
2023-02-01 13:31:55 -0300

    Fix issues and add Link to curriculum page

--- assets/js/typingAnimation.js
+++ assets/js/typingAnimation.js
@@ -1,12 +1,17 @@
 const h1 = document.querySelector("h1");
-const text = "Hello World ! ";
+const text = "Hello World!";
 let i = 0;
+let cursor = "_";
 
 const typing = setInterval(() => {
   if (i < text.length) {
-    h1.innerHTML += text.charAt(i);
+    h1.innerHTML = text.slice(0, i) + cursor;
     i++;
+    setTimeout(typing, Math.random() * 100 + 50);
   } else {
     clearInterval(typing);
-  }
-}, 100);
+    h1.innerHTML = text;
+  }
+
+  cursor = cursor === "_" ? "" : "_";
+}, 100);
